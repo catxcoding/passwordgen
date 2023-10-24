e3d50de18d019ce9b91f67e29d608391fdcae752
catxcoding <145952390+catxcoding@users.noreply.github.com>
2023-10-23 21:28:47 -0700

    Fixed error from console

--- assets/script.js
+++ assets/script.js
@@ -21,60 +21,33 @@ function generatePassword() {
 
   let passwordLength = numBox.value;
   console.log(numBox.value);
-}
+
   //Checks if uppercase, lowercase, etc boxes are selected
-  if (upperBtn.checked) {
+  if (upperBtn && upperBtn.checked) {
     console.log("Upper Case Button Selected: " + upperBtn.checked);
     charSet += chars.upperCase;
   }
-  if (lowerBtn.checked) {
+  if (lowerBtn && lowerBtn.checked) {
     console.log("Lower Case Button Selected: " + lowerBtn.checked);
     charSet += chars.lowerCase;
   }
-  if (numberBtn.checked) {
+  if (numberBtn && numberBtn.checked) {
     console.log("Number Button Selected: " + numberBtn.checked);
     charSet += chars.num;
   }
-  if (specialBtn.checked) {
+  if (specialBtn && specialBtn.checked) {
     console.log("Special Character Button Selected: " + specialBtn.checked);
     charSet += chars.special;
   }
 
-//If empty password length, if no buttons are checked, website will alert user
-if (
-  !specialBtn.checked &&
-  !numberBtn.checked &&
-  !lowerBtn.checked &&
-  !upperBtn.checked
-) {
-  alert("No boxes selected! Please choose one or more boxes.");
-}
-if (!passwordLength) {
-  alert(
-    "No Password Length selected! Please input number."
-  );
-}
-if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  alert("Password Length must be between 8 and 128 Characters! Please input valid number.");
-  numBox.dataset.value = '';
-  passwordLength = 0;
-}
-else if (passwordLength > 128) { // Move the else keyword to the correct position
-  alert("Password Length more than 128 Characters! Please input valid number.");
-  numBox.dataset.value = '';
-  passwordLength = 0;
-}
-if (!passwordLength || passwordLength < 1) {
-  alert("Password Length must be a positive number! Please input valid number.");
-  numBox.dataset.value = '';
-  passwordLength = 0;
-}
-
-for (i = 0; i < passwordLength; i++) {
-  passwordResult += charSet.charAt(
-    Math.floor(Math.random() * charSet.length)
-    );
+  // generate password
+  for (let i = 0; i < passwordLength; i++) {
+    passwordResult += charSet.charAt(Math.floor(Math.random() * charSet.length));
   }
+
+  // return generated password
+  return passwordResult;
+}
 
   function generatePassword() {
     let passwordResult = "";
