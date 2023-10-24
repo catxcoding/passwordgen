8d9a561f994793c99ed8f6f290f6b0fe707c4dd5
catxcoding <145952390+catxcoding@users.noreply.github.com>
2023-10-23 19:08:58 -0700

    added elements and characters

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
-
+const generateBtn = document.querySelector("#generate");
+const copyBtn = document.getElementById("copy");
+const upperBtn = document.getElementById("upperCaseCheck");
+const lowerBtn = document.getElementById("lowerCaseCheck");
+const numberBtn = document.getElementById("numbersCheck");
+const specialBtn = document.getElementById("specialCheck");
+let numBox = document.getElementById("pass-length-box");
 var generateBtn = document.querySelector("#generate");
+
+const chars = {
+  lowerCase: "qwertyuioplkjhgfdsazxcvbnm",
+  upperCase: "QWERTYUIOPLKJHGFDSAZXCVBNM",
+  num: "1234567890",
+  special: "!@#$%^&*()",
+};
 
 // Write password to the #password input
 function writePassword() {
